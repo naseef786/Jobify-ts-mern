54ca96489495cc3a5effe198459e217c5583ec31
naseef786 <naseefac786@gmail.com>
2023-08-11 19:22:08 +0530

    search box completed

---
+++ client/src/components/search/Search.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { AiOutlineCloseCircle, AiOutlineSearch } from 'react-icons/ai';
+import { BsHouseDoor } from 'react-icons/bs'
+import { CiLocationOn } from 'react-icons/ci'
+
+
+const SearchBox = () => {
+    return (
+        <div className='searchDiv grid gap-10  bg-gradient-to-br bg-slate-300 rounded-[10px] p-[3rem] '>
+            <form action="" >
+                <div className='first flex justify-between items-center rounded-[8px] gap-[10px] bg-white shadow-lg shadow-grayish-700'>
+                    <div className='flex gap-2 items-center  h-24'>
+
+                        <AiOutlineSearch className="text-[25px] icon" />
+                        <input
+                            type="text"
+                            className="bg-transparent text-blue-500 border border-blue-500 border-solid border-opacity-50 focus:outline-none w-full py-2 px-4 rounded-md"
+                            placeholder="Search Job Here ..."
+                        />
+                        <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon ' />
+                    </div>
+                    <div className='flex gap-2 items-center '>
+
+                        <BsHouseDoor className="text-[25px] icon" />
+                        <input
+                            type="text"
+                            className="bg-transparent text-blue-500 border border-blue-500 border-solid border-opacity-50 focus:outline-none w-full py-2 px-4 rounded-md"
+                            placeholder="Searcc By Company"
+                        />
+                        <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon ' />
+                    </div>
+                    <div className='flex gap-2 items-center '>
+
+                        <CiLocationOn className="text-[25px] icon" />
+                        <input
+                            type="text"
+                            className="bg-transparent text-blue-500 border border-blue-500 border-solid border-opacity-50 focus:outline-none w-full py-2 px-4 rounded-md"
+                            placeholder="Search By Location"
+                        />
+
+                        <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon ' />
+                    </div>
+                    <button className='bg-blue-600 h-full p-2 text-white m-3 px-10 rounded-[10px]  cursor-pointer hover:bg-blue-400'>
+                        Search
+                    </button>
+
+                </div>
+            </form>
+            <div className='secDiv flex items-center gap-10 justify-center'>
+
+                <div className="singleSearch flex items-center gap-2">
+                    <label htmlFor="relevance" className="text-[#808080] font-semibold">
+                        Sort by:
+                        <select
+                            name="relevance"
+                            id="relevance"
+                            className=" w-36 border border-gray-300 rounded-md p-1 focus:outline-none focus:ring focus:border-blue-300"
+                        >
+                            <option value="">Relevance</option>
+                            <option value="">Inclusive</option>
+                            <option value="">Starts With</option>
+                            <option value="">Contains</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="singleSearch flex items-center gap-2">
+                    <label htmlFor="relevance" className="text-[#808080] font-semibold">
+                        Sort by:
+                        <select
+                            name="relevance"
+                            id="relevance"
+                            className=" w-36 border border-gray-300 rounded-md p-1 focus:outline-none focus:ring focus:border-blue-300"
+                        >
+                            <option value="">Relevance</option>
+                            <option value="">Inclusive</option>
+                            <option value="">Starts With</option>
+                            <option value="">Contains</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="singleSearch flex items-center gap-2">
+                    <label htmlFor="relevance" className="text-[#808080] font-semibold">
+                        Sort by:
+                        <select
+                            name="relevance"
+                            id="relevance"
+                            className=" w-36 border border-gray-300 rounded-md p-1 focus:outline-none focus:ring focus:border-blue-300"
+                        >
+                            <option value="">Relevance</option>
+                            <option value="">Inclusive</option>
+                            <option value="">Starts With</option>
+                            <option value="">Contains</option>
+                        </select>
+                    </label>
+                </div>
+
+
+
+            </div>
+        </div>
+    )
+}
+
+export default SearchBox
--- client/src/pages/candidate/JobsPage.tsx
+++ client/src/pages/candidate/JobsPage.tsx
@@ -3,11 +3,13 @@ import NavBar from '../../components/header/Navbar'
 import Jobs from '../../components/jobs/Jobs'
 import Footer from '../../components/footer/Footer'
 import TileNav from '../../components/header/TileNav'
+import SearchBox from '../../components/search/Search'
 
 function JobsPage() {
   return (
     <>
     <TileNav/>
+    <SearchBox/>
     <Jobs/>
     <Footer/>
     </>
